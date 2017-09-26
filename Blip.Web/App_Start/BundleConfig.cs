1f3f4109e1218cb3df350b88c273293c9a80558b
ajsaulsberry <ajsaulsberry@gmail.com>
2017-09-26 14:37:23 -0400

    All JavaScript where is should be and functioning properly. No duplicate loads. Note that .change function bound to controls on partial view CreatePostalAddressPartial.cshtml has to be included on the partial view to be bound when it is loaded.

--- Blip.Web/App_Start/BundleConfig.cs
+++ Blip.Web/App_Start/BundleConfig.cs
@@ -12,8 +12,11 @@ namespace BlipProjects
                         "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.unobtrusive*", 
                         "~/Scripts/jquery.validate*"));
+
+            // jqueryunobtrusive includes unobtrusive-ajax but not validate.unobtrusive.
+            bundles.Add(new ScriptBundle("~/bundles/jqueryunobtrusive").Include(
+                        "~/Scripts/jquery.unobtrusive*"));
 
             // Use the development version of Modernizr to develop with and learn from. Then, when you're
             // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
